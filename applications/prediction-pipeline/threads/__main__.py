9436fbe8179ca2d14bb23d8f6b78a4850c758c07
jjkotni <kjjswaroop@gmail.com>
2020-03-31 20:47:28 +0530

    Prediction pipeline native threads

---
+++ applications/prediction-pipeline/threads/__main__.py
@@ -0,0 +1,51 @@
+import os
+import threading
+
+from predict import predictHandler
+from resize import resizeHandler
+from render import renderHandler
+
+resizeOut  = {}
+predictOut = {}
+renderOut  = {}
+
+def resizeWorker(event):
+    result = resizeHandler(event)
+
+    global resizeOut
+    resizeOut = result
+
+def predictWorker():
+    global resizeOut
+
+    result = predictHandler(resizeOut)
+
+    global predictOut
+    predictOut = result
+
+def renderWorker():
+    global predictOut
+
+    result = renderHandler(predictOut)
+
+    global renderOut
+    renderOut = result
+
+def main(event):
+    resize  = threading.Thread(target=resizeWorker, args=[event])
+    predict = threading.Thread(target=predictWorker)
+    render  = threading.Thread(target=renderWorker)
+
+    resize.start()
+    resize.join()
+
+    predict.start()
+    predict.join()
+
+    render.start()
+    render.join()
+
+    return renderOut
+
+# if __name__ == "__main__":
+#     main({})
